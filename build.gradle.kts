9509e30eefed6a9bcb2b1ce64d6c9d0942c52775
Roman Makeev <makeevrserg@gmail.com>
2023-05-01 08:35:55 +0300

    - added version creation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("java")
     id("com.github.johnrengelman.shadow") version "7.1.2"
@@ -22,5 +24,17 @@ tasks.shadowJar {
     dependsOn(configurations)
     archiveClassifier.set(null as String?)
     archiveBaseName.set("JVMGithubActions")
-    destinationDirectory.set(File(rootDir,"jars").also(File::mkdirs))
+    destinationDirectory.set(File(rootDir, "jars").also(File::mkdirs))
+}
+
+tasks.create("PublishPrimaryVersion") {
+    val file = File("./.github/version.env")
+    if (!file.exists()) {
+        file.parentFile.mkdirs()
+        file.createNewFile()
+    }
+    val properties = Properties()
+    properties.load(file.reader())
+    properties.setProperty("MAJOR_VERSION", "1.0.0")
+    properties.store(file.writer(), null)
 }
